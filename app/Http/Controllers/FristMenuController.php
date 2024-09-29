b05919fde92a2181b086b79ed519dfe4b7d173d4
Reza HajRahimi <rezahajrahimi@yahoo.com>
2024-09-29 09:55:04 +0330

    fix fuplicate start command text fix referral user on himself bug

--- app/Http/Controllers/FristMenuController.php
+++ app/Http/Controllers/FristMenuController.php
@@ -1,3 +1,3 @@
 <?php
-bolt_decrypt( __FILE__ , '5ybYic'); return 0;
-##!!!##Pz+jlqKaqKWWmJpVdqWlkX2pqaWReKSjqaekoaGap6hwPz+qqJpVfqGhqqKeo5apmpF9qamlkYeapqqaqKlwPz+YoZaoqFV7p56oqYKao6p4pKOpp6ShoZqnVZqtqZqjmahVeKSjqaekoaGapz+wP1VVVVVkZD+yPw==
+bolt_decrypt( __FILE__ , 'PMNzRL'); return 0;
+##!!!##DQ1xZHBodnNkZmgjRHNzX0t3d3NfRnJxd3Vyb29odXY+DQ14dmgjTG9veHBscWR3aF9Ld3dzX1VodHhodnc+DQ1mb2R2diNJdWx2d1BocXhGcnF3dXJvb2h1I2h7d2hxZ3YjRnJxd3Vyb29odQ1+DSMjIyMyMg2ADQ==
--- app/Models/Telegram.php
+++ app/Models/Telegram.php
@@ -1,3 +1,3 @@
 <?php
-bolt_decrypt( __FILE__ , '7sMQsR'); return 0;
-##!!!##Fxd7bnpygH1ucHItTn19aVp8cXJ5gEgXF4KAci1WeXmCenZ7boFyaVFugW5vboByaVJ5fH6CcnuBaVNucIF8f3ZygGlVboBTbnCBfH+GSBeCgHItVnl5gnp2e26BcmlRboFub26AcmlSeXx+gnJ7gWlafHFyeUgXF3B5boCALWFyeXJ0f256LXKFgXJ7cYAtWnxxcnkXiBctLS0tgoByLVVugFNucIF8f4ZIF4oX
+bolt_decrypt( __FILE__ , 'uubjNB'); return 0;
+##!!!##UFC0p7Orubanqatmh7a2opO1qquyuYFQULu5q2aPsrK7s6+0p7qrooqnuqeop7mroouytbe7q7S6ooynqbq1uK+ruaKOp7mMp6m6tbi/gVC7uatmj7Kyu7OvtKe6q6KKp7qnqKe5q6KLsrW3u6u0uqKTtaqrsoFQUKmyp7m5ZpqrsqutuKezZqu+uqu0qrlmk7Wqq7JQwVBmZmZmu7mrZo6nuYynqbq1uL+BUMNQ

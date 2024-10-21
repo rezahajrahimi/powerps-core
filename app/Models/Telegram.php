edc60640392e95767b4b2fada0cfa0220944a47a
Reza HajRahimi <rezahajrahimi@yahoo.com>
2024-10-21 16:52:27 +0330

    fix folders permisson

--- app/Models/Telegram.php
+++ app/Models/Telegram.php
@@ -1,3 +1,3 @@
 <?php
-bolt_decrypt( __FILE__ , '1T2pSC'); return 0;
-##!!!##x8crHioiMC0eICLd/i0tGQosISIpMPjHxzIwIt0GKSkyKiYrHjEiGQEeMR4fHjAiGQIpLC4yIisxGQMeIDEsLyYiMBkFHjADHiAxLC82+McyMCLdBikpMiomKx4xIhkBHjEeHx4wIhkCKSwuMiIrMRkKLCEiKfjHxyApHjAw3REiKSIkLx4q3SI1MSIrITDdCiwhIinHOMfd3d3dMjAi3QUeMAMeIDEsLzb4xzrH
+bolt_decrypt( __FILE__ , 'U3axuu'); return 0;
+##!!!##VVW5rLiwvrusrrBrjLu7p5i6r7C3voZVVcC+sGuUt7fAuLS5rL+wp4+sv6ytrL6wp5C3urzAsLm/p5Gsrr+6vbSwvqeTrL6RrK6/ur3EhlXAvrBrlLe3wLi0uay/sKePrL+sray+sKeQt7q8wLC5v6eYuq+wt4ZVVa63rL6+a5+wt7Cyvay4a7DDv7C5r75rmLqvsLdVxlVra2trwL6wa5OsvpGsrr+6vcSGVchV

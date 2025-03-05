9c29538eda1d3453de3cce2ef74761fd76d19b78
Reza <rezahajrahimi@gmail.com>
2025-03-05 12:16:03 +0330

    fix start command bugs on new text style

--- app/Models/TestAccountMenu.php
+++ app/Models/TestAccountMenu.php
@@ -1,3 +1,3 @@
 <?php
-bolt_decrypt( __FILE__ , 'thjiLy'); return 0;
-##!!!##fn7i1eHZ5+TV19mUteTk0MHj2Nng569+funn2ZS94ODp4d3i1ejZ0LjV6NXW1efZ0Lng4+Xp2eLo0LrV1+jj5t3Z59C81ee61dfo4+btr37p59mUveDg6eHd4tXo2dC41ejV1tXn2dC54OPl6dni6NDB49jZ4K9+ftfg1efnlMjZ5+i119fj6eLowdni6ZTZ7OjZ4tjnlMHj2Nngfu9+lJSUlOnn2ZS81ee61dfo4+btr37xfg==
+bolt_decrypt( __FILE__ , 'cchcBU'); return 0;
+##!!!##MjKWiZWNm5iJi41IaZiYhHWXjI2Um2MyMp2bjUhxlJSdlZGWiZyNhGyJnImKiZuNhG2Ul5mdjZachG6Ji5yXmpGNm4RwiZtuiYucl5qhYzKdm41IcZSUnZWRlomcjYRsiZyJiombjYRtlJeZnY2WnIR1l4yNlGMyMouUiZubSHyNm5xpi4uXnZacdY2WnUiNoJyNloybSHWXjI2UMqMySEhISJ2bjUhwiZtuiYucl5qhYzKlMg==
